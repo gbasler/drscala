4aeeeca603667905e3a4c99198570720c4d84715
Alois Cochard <alois.cochard@gmail.com>
2013-10-25 23:34:50 +0100

    Add Foo.

---
+++ src/main/scala/Foo.scala
@@ -0,0 +1,44 @@
+package foo
+
+object Bar {
+  def value[T] = 42.asInstanceOf[T]
+
+  def first[T](xs: Seq[T]) = xs.head
+
+  def firstOpt[T](xs: Option[T]) = xs.head
+
+
+
+
+
+
+
+
+  def run = ???
+
+  println("HelloWorld")
+  
+  def oget[T](o: Option[T]) = o.get
+
+  def ohno = value
+
+  def uglyline = "what's wrong?";
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
