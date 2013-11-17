fbf464ea7624ca5fac1cb2955b17dd44cf41da02
Gerard Basler <gerard.basler@gmail.com>
2013-11-17 22:22:28 +0100

    Add more warnings for `Option.get.getOrElse`, `Map.find`, `Set.find`.

---
+++ main/src/main/scala/Foo.scala
@@ -0,0 +1,25 @@
+package foo
+
+object Bar {
+  //  def oget[T](o: Option[T]) = o.get
+
+  //  def mget[K, V](o: Map[K, V], k: K, d: V) = o.get(k).getOrElse(d)
+
+  def mapfind[K, V](o: Map[K, V], k: K) = o.find(_._1 == k)
+
+  def listfind[K](o: List[K], k: K) = o.find(_ == k)
+
+  def NoType = "hello"
+
+}
+
+
+
+
+
+
+
+
+
+
+
--- plugin/src/main/scala/doctors/StdLib.scala
+++ plugin/src/main/scala/doctors/StdLib.scala
@@ -1,6 +1,8 @@
 package drscala
 package doctors
 
+import scala.tools.nsc.Global
+
 trait StdLibComponent { self: HealthCake =>
   import self.global._
 
@@ -14,14 +16,12 @@ trait StdLibComponent { self: HealthCake =>
       case DefDef(_, _, _, _, tpt, _) if tpt.exists(_.tpe =:= typeOf[Nothing]) => true
     }
 
+    private def isMap(tpe: Type) = tpe.typeSymbol.isNonBottomSubClass(typeOf[Map[Any, Any]].typeSymbol)
+
+    private def isSet(tpe: Type) = tpe.typeSymbol.isNonBottomSubClass(typeOf[Set[Any]].typeSymbol)
+
     override val diagnostic: PartialFunction[PhaseId, CompilationUnit => Seq[(Position, Message)]] = {
       case "parser" => _.body.collect {
-        case tree@Ident(name) if name.toString == "$qmark$qmark$qmark" =>
-          tree -> "Oops, an implementation is missing here."
-
-        case tree@Apply(Ident(name), _) if name.toString == "println" =>
-          tree -> "There is rarely a good reason to use `println`, is it the case here?"
-
         case tree@Select(_, name) if name.toString == "asInstanceOf" =>
           tree -> "There should be a better way than using `asInstanceOf`, what do you think?"
       }
@@ -35,34 +35,25 @@ trait StdLibComponent { self: HealthCake =>
             s"Because calling `$name` might throw an exception in this case."
           )
 
+        case tree@Select(value, name) if name.toString == "get" && value.tpe <:< typeOf[Option[Any]] =>
+          tree -> "There is surely a better way than calling `Option.get`, any idea?"
+
         case tree@Select(value@Apply(Select(ident, n1), _), n2)
           if n1.toString == "get" && n2.toString == "getOrElse" && value.tpe <:< typeOf[Option[Any]] =>
           tree -> s"`$ident.get.getOrElse` are you serious? Try `$ident.getOrElse` instead."
 
-        case tree@Select(value, name) if name.toString == "find" && value.tpe.baseClasses.exists(_.tpe.toString().contains("scala.collection.Map")) =>
+        case tree@Select(value, name) if name.toString == "find" && isMap(value.tpe) =>
           tree -> s"`find` on a `Map`, you are joking???"
 
-        case tree@Select(value, name) if name.toString == "find" && value.tpe.baseClasses.exists(_.tpe.toString().contains("scala.collection.Set")) =>
+        case tree@Select(value, name) if name.toString == "find" && isSet(value.tpe) =>
           tree -> s"`find` on a `Set`, you are joking???"
 
-        case tree@Select(v@Apply(value@Select(ident, n1), a1), n2) if n1.toString == "get" && n2.toString == "getOrElse" =>
-          println(ident.tpe <:< typeOf[scala.collection.Map[Any, Any]])
-          println(ident.tpe <:< typeOf[scala.Predef.Map[Any, Any]])
-          println(show(ident.tpe))
-        import BooleanFlag._
-          println(showRaw(ident.tpe, printTypes = true))
-          println(showRaw(value.tpe, printTypes = true))
-          println(showRaw(tree.tpe, printTypes = true))
-          println(show(ident))
-          println(showRaw(ident))
-          println(show(tree))
-          println("!"+showRaw(tree,printTypes = true))
-        println( tree.tpe <:< typeOf[Option[Any]])
-        println( v.tpe <:< typeOf[Option[Any]])
-          tree -> s"wtf $n1 $a1 $n2 $ident ${ident.tpe}"
-
-        case tree@Select(value, name) if name.toString == "get" /*&& value.tpe <:< typeOf[scala.collection.Map[Any, Any]]*/ =>
-          tree -> s"`Map.get`, any idea? ${value.summaryString} ${value.tpe.toString()}"
+//        case tree@Select(v@Apply(value@Select(ident, n1), a1), n2) if n1.toString == "get" && n2.toString == "getOrElse" =>
+//          println(ident.tpe <:< typeOf[scala.collection.Map[Any, Any]])
+//          println(ident.tpe <:< typeOf[scala.Predef.Map[Any, Any]])
+//          println(show(ident.tpe))
+//          import BooleanFlag._
+//          println(showRaw(ident.tpe, printTypes = true))
       }
     }
 
