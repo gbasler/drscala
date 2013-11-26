77c58fb83ef66e071d647e98efd44e86fc76cb85
Gerard Basler <gerard.basler@efgfp.com>
2013-11-26 10:16:27 +0100

    Simplifieable operation on collection.

--- plugin/src/main/scala/doctors/StdLib.scala
+++ plugin/src/main/scala/doctors/StdLib.scala
@@ -51,57 +51,69 @@ trait StdLibComponent {
         case tree@Select(_, name) if name.toString == "asInstanceOf" =>
           tree -> "An `asInstanceOf` could result in a `ClassCastException` at runtime, it's better to use a pattern match."
       }
-      case "typer" => _.body.collect {
-        case tree if isNothingInferred(tree) =>
-          tree -> "I feel a disturbance in the force, the type `Nothing` might have been inferred."
+      case "typer" =>
+        cu =>
+          println(cu)
 
-        // TODO: remove, it's too noisy
-        case tree@Select(value, name) if unsafeOnEmptyIterable.contains(name.toString) && value.tpe <:< typeOf[Iterable[Any]] =>
-          tree -> (
-            s"Are you sure the `${value.tpe.typeSymbol.name}` will never be empty?\n" +
-              s"Because calling `$name` might throw an exception in this case."
-            )
+          cu.body.collect {
+            case tree if isNothingInferred(tree) =>
+              tree -> "I feel a disturbance in the force, the type `Nothing` might have been inferred."
 
-        case tree@Select(ident, name) if name.toString == "get" && ident.tpe <:< typeOf[Option[Any]] =>
-          tree -> s"""`$ident.get` can result in a `NoSuchElementException`, I recommend to write `$ident.getOrElse(sys.error("..."))`"""
+            // TODO: remove, it's too noisy
+            //        case tree@Select(value, name) if unsafeOnEmptyIterable.contains(name.toString) && value.tpe <:< typeOf[Iterable[Any]] =>
+            //          tree -> (
+            //            s"Are you sure the `${value.tpe.typeSymbol.name}` will never be empty?\n" +
+            //              s"Because calling `$name` might throw an exception in this case."
+            //            )
 
-        case tree@Select(value@Apply(Select(ident, n1), _), n2)
-          if n1.toString == "get" && n2.toString == "getOrElse" && value.tpe <:< typeOf[Option[Any]] =>
-          tree -> s"""`$ident.get(...).getOrElse(...)` can be simplified to `$ident.getOrElse(...)`."""
+            case tree@Select(ident, name) if name.toString == "get" && ident.tpe <:< typeOf[Option[Any]] =>
+              tree -> s"""`$ident.get` can result in a `NoSuchElementException`, I recommend to write `$ident.getOrElse(sys.error("..."))`"""
 
-        case tree@Select(ident, name) if name.toString == "find" && isMap(ident.tpe) =>
-          tree -> s"`find` on a `Map` is O(n), you should use `$ident.get` instead."
+            case tree@Select(value@Apply(Select(ident, n1), _), n2)
+              if n1.toString == "get" && n2.toString == "getOrElse" && value.tpe <:< typeOf[Option[Any]] =>
+              tree -> s"""`$ident.get(...).getOrElse(...)` can be simplified to `$ident.getOrElse(...)`."""
 
-        case tree@Select(ident, name) if name.toString == "find" && isSet(ident.tpe) =>
-          tree -> s"`find` on a `Set` is O(n), you should use `$ident.get` instead."
+            case tree@Select(value@Apply(Select(ident, n1), _), n2)
+              if n1.toString == "map" && n2.toString == "getOrElse" && value.tpe <:< typeOf[Option[Any]] =>
+              tree -> s"""`$ident.get(...).getOrElse(...)` can be simplified to `$ident.getOrElse(...)`."""
 
-        case tree@CaseClassArrayMembersExtractor((ident, members)) =>
-          val names = members.map(_.name)
-          tree -> (s"""`$ident`.{`${names.mkString(",")}`}: case class with `Array`s in c'tor: """ +
-            s"""structural equality / hashing is not implemented. Use either `mutable.WrappedArray` or `IndexedSeq`.""")
+            case tree@Select(ident, name) if name.toString == "find" && isMap(ident.tpe) =>
+              tree -> s"`find` on a `Map` is O(n), you should use `$ident.get` instead."
 
-        case tree@If(cond, Literal(Constant(true)), Literal(Constant(false))) =>
-          tree -> s"`${briefTree(tree)}` can be simplified to `${briefTree(cond)}`."
+            case tree@Select(ident, name) if name.toString == "find" && isSet(ident.tpe) =>
+              tree -> s"`find` on a `Set` is O(n), you should use `$ident.get` instead."
 
-        case tree@If(cond, Literal(Constant(false)), Literal(Constant(true))) =>
-          tree -> s"`${briefTree(tree)}` can be simplified to `!${briefTree(cond)}`."
+            case tree@CaseClassArrayMembersExtractor((ident, members)) =>
+              val names = members.map(_.name)
+              tree -> (s"""`$ident`.{`${names.mkString(",")}`}: case class with `Array`s in c'tor: """ +
+                s"""structural equality / hashing is not implemented. Use either `mutable.WrappedArray` or `IndexedSeq`.""")
 
-//        case tree@Select(value, name) if name.toString == "foreach" =>
-//          println(tree.tpe)
-//          println(tree.tpe.prefix)
-//          println(tree.tpe.prefixChain)
-//          println(tree.tpe.typeSymbol)
-//          println(tree.tpe <:< typeOf[Range])
-//          println(tree.tpe <:< typeOf[scala.collection.immutable.Range])
-//          tree -> s"foreach"
+            case tree@If(cond, Literal(Constant(true)), Literal(Constant(false))) =>
+              tree -> s"`${briefTree(tree)}` can be simplified to `${briefTree(cond)}`."
 
-        //        case tree@Select(v@Apply(value@Select(ident, n1), a1), n2) if n1.toString == "get" && n2.toString == "getOrElse" =>
-        //          println(ident.tpe <:< typeOf[scala.collection.Map[Any, Any]])
-        //          println(ident.tpe <:< typeOf[scala.Predef.Map[Any, Any]])
-        //          println(show(ident.tpe))
-        //          import BooleanFlag._
-        //          println(showRaw(ident.tpe, printTypes = true)))
-      }
+            case tree@If(cond, Literal(Constant(false)), Literal(Constant(true))) =>
+              tree -> s"`${briefTree(tree)}` can be simplified to `!${briefTree(cond)}`."
+
+            case tree@Select(Apply(TypeApply(Select(ident, name1), typeArg :: Nil), arg :: Nil), name2)
+              if name1.toString == "map" && name2.toString == "getOrElse" && typeArg.tpe =:= typeOf[Boolean] =>
+              tree -> s"Simplifiable operation on collection: `$ident.exists(${briefTree(arg)}})`"
+
+            //        case tree@Select(value, name) if name.toString == "foreach" =>
+            //          println(tree.tpe)
+            //          println(tree.tpe.prefix)
+            //          println(tree.tpe.prefixChain)
+            //          println(tree.tpe.typeSymbol)
+            //          println(tree.tpe <:< typeOf[Range])
+            //          println(tree.tpe <:< typeOf[scala.collection.immutable.Range])
+            //          tree -> s"foreach"
+
+            //        case tree@Select(v@Apply(value@Select(ident, n1), a1), n2) if n1.toString == "get" && n2.toString == "getOrElse" =>
+            //          println(ident.tpe <:< typeOf[scala.collection.Map[Any, Any]])
+            //          println(ident.tpe <:< typeOf[scala.Predef.Map[Any, Any]])
+            //          println(show(ident.tpe))
+            //          import BooleanFlag._
+            //          println(showRaw(ident.tpe, printTypes = true)))
+          }
     }
 
     override val examine: Seq[(String, Column => Position)] => Seq[(Position, Message)] = xs => {
