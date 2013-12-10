f5a2c6340957449981a3ac2572ac866669e4fcb4
Gerard Basler <gerard.basler@efgfp.com>
2013-12-10 08:36:31 +0100

    Fixed: warn only about non return type annotation if method is not inherited.

--- plugin/src/main/scala/doctors/StdLib.scala
+++ plugin/src/main/scala/doctors/StdLib.scala
@@ -3,7 +3,6 @@ package doctors
 
 import scala.tools.nsc.Global
 import scala._
-import scala.Some
 
 trait StdLibComponent {
   self: HealthCake =>
@@ -65,45 +64,41 @@ trait StdLibComponent {
       }
     }
 
+    private def checkNoReturnType(tree: Tree): Seq[((Line, Column), Message)] = {
 
-    object CheckNoReturnType {
-      def unapply(tree: Tree) = {
-
-        def overridden(name: Name, baseClasses: List[Symbol], vparamss: List[List[ValDef]]): Boolean = {
-          baseClasses.exists {
-            symbol: Symbol =>
-              symbol.tpe.members.exists {
-                s: Symbol =>
-                  s.name == name && s.tpe.paramss == vparamss
-              }
-          }
-        }
-
-        def check(baseClasses: List[Symbol], impl: Template) = {
-          impl.body.collect {
-            case tree@DefDef(Modifiers(0L, tpnme.EMPTY, _), name, tparams, vparamss, tpt: TypeTree, rhs)
-              if !tree.symbol.isConstructor && tree.symbol.isPublic &&
-                tpt.original == null &&
-                !(tpt.tpe =:= typeOf[Unit]) /*&&
-                overridden(name, baseClasses, vparamss)*/
-                 =>
-
-              println(s"$name ${overridden(name, baseClasses, vparamss)} ${vparamss}")
-
-              tree -> s"The `public` method `$name` should have explicit return type, `$tpt` was inferred. Please specify return type."
-          }
-        }
-
-        tree match {
-          case tree@ClassDef(mods, name, tparams, impl) =>
-            check(tree.symbol.baseClasses, impl)
-
-          case tree@ModuleDef(mods, name, impl) =>
-            check(tree.symbol.baseClasses, impl)
-
-          case _ => Nil
+      def overridden(name: Name, baseClasses: List[Symbol], vparamss: List[List[ValDef]]): Boolean = {
+        baseClasses.exists {
+          symbol: Symbol =>
+            symbol.tpe.members.exists {
+              s: Symbol =>
+                s.name == name && s.tpe.paramss == vparamss
+            }
         }
       }
+
+      def check(baseClasses: List[Symbol], impl: Template): Seq[(Position, String)] = {
+        impl.body.collect {
+          case tree@DefDef(Modifiers(0L, tpnme.EMPTY, _), name, tparams, vparamss, tpt: TypeTree, rhs)
+            if !tree.symbol.isConstructor && tree.symbol.isPublic &&
+              tpt.original == null &&
+              !(tpt.tpe =:= typeOf[Unit]) &&
+                overridden(name, baseClasses, vparamss) =>
+
+            (tree.pos.line, tree.pos.column) -> s"The `public` method `$name` should have explicit return type, `$tpt` was inferred. Please specify return type."
+        }
+      }
+
+      val c = tree.collect {
+        case tree@ClassDef(mods, name, tparams, impl) =>
+          check(tree.symbol.baseClasses, impl).toSeq
+
+        case tree@ModuleDef(mods, name, impl) =>
+          check(tree.symbol.baseClasses, impl).toSeq
+
+        case _ =>
+          Seq.empty[(Position, String)]
+      }
+      c.flatten
     }
 
     override val diagnostic: PartialFunction[PhaseId, CompilationUnit => Seq[(Position, Message)]] = {
@@ -112,64 +107,67 @@ trait StdLibComponent {
           tree -> "An `asInstanceOf` could result in a `ClassCastException` at runtime, it's better to use a pattern match."
       }
       case "typer" =>
-        u => CheckNoReturnType.unapply(u.body).toSeq ++ u.body.collect {
-        case tree if isNothingInferred(tree) =>
-          tree -> "I feel a disturbance in the force, the type `Nothing` might have been inferred."
+        u =>
+          val classChecks: Seq[(Position, Message)] = checkNoReturnType(u.body)
+          val bodyChecks: Seq[(Position, Message)] = u.body.collect {
+            case tree if isNothingInferred(tree) =>
+              tree -> "I feel a disturbance in the force, the type `Nothing` might have been inferred."
 
-        case tree@Select(value@Apply(Select(ident, n1), _), n2)
-          if n1.toString == "get" && n2.toString == "getOrElse" && value.tpe <:< typeOf[Option[Any]] =>
-          tree -> s"""`$ident.get(...).getOrElse(...)` can be simplified to `$ident.getOrElse(...)`."""
+            case tree@Select(value@Apply(Select(ident, n1), _), n2)
+              if n1.toString == "get" && n2.toString == "getOrElse" && value.tpe <:< typeOf[Option[Any]] =>
+              tree -> s"""`$ident.get(...).getOrElse(...)` can be simplified to `$ident.getOrElse(...)`."""
 
-        case tree@Select(Apply(TypeApply(Select(ident, name1), typeArg :: Nil), arg :: Nil), name2)
-          if name1.toString == "map" && name2.toString == "getOrElse" && typeArg.tpe =:= typeOf[Boolean] =>
-          tree -> s"Simplifiable operation on collection, rewrite to: `$ident.exists(${briefTree(arg)}})`"
+            case tree@Select(Apply(TypeApply(Select(ident, name1), typeArg :: Nil), arg :: Nil), name2)
+              if name1.toString == "map" && name2.toString == "getOrElse" && typeArg.tpe =:= typeOf[Boolean] =>
+              tree -> s"Simplifiable operation on collection, rewrite to: `$ident.exists(${briefTree(arg)}})`"
 
-        case tree@Select(ident, name) if name.toString == "find" && isMap(ident.tpe) =>
-          tree -> s"`find` on a `Map` is O(n), you should use `$ident.get` instead."
+            case tree@Select(ident, name) if name.toString == "find" && isMap(ident.tpe) =>
+              tree -> s"`find` on a `Map` is O(n), you should use `$ident.get` instead."
 
-        case tree@Select(ident, name) if name.toString == "find" && isSet(ident.tpe) =>
-          tree -> s"`find` on a `Set` is O(n), you should use `$ident.get` instead."
+            case tree@Select(ident, name) if name.toString == "find" && isSet(ident.tpe) =>
+              tree -> s"`find` on a `Set` is O(n), you should use `$ident.get` instead."
 
-        case tree@CaseClassArrayMembersExtractor((ident, members)) =>
-          val names = members.map(_.name)
-          tree -> (s"""`$ident`.{`${names.mkString(",")}`}: case class with `Array`s in c'tor: """ +
-            s"""structural equality / hashing is not implemented. Use either `mutable.WrappedArray` or `IndexedSeq`.""")
+            case tree@CaseClassArrayMembersExtractor((ident, members)) =>
+              val names = members.map(_.name)
+              tree -> (s"""`$ident`.{`${names.mkString(",")}`}: case class with `Array`s in c'tor: """ +
+                s"""structural equality / hashing is not implemented. Use either `mutable.WrappedArray` or `IndexedSeq`.""")
 
-        case tree@If(cond, Literal(Constant(true)), Literal(Constant(false))) =>
-          tree -> s"`${briefTree(tree)}` can be simplified to `${briefTree(cond)}`."
+            case tree@If(cond, Literal(Constant(true)), Literal(Constant(false))) =>
+              tree -> s"`${briefTree(tree)}` can be simplified to `${briefTree(cond)}`."
 
-        case tree@If(cond, Literal(Constant(false)), Literal(Constant(true))) =>
-          tree -> s"`${briefTree(tree)}` can be simplified to `!${briefTree(cond)}`."
+            case tree@If(cond, Literal(Constant(false)), Literal(Constant(true))) =>
+              tree -> s"`${briefTree(tree)}` can be simplified to `!${briefTree(cond)}`."
 
-//        case tree@CheckNoReturnType(t, msg) =>
-//          t -> msg
+            //        case tree@CheckNoReturnType(t, msg) =>
+            //          t -> msg
 
-        // TODO: complain only for functions that are not already in tpt.tpe.baseType...
-//        case tree@DefDef(Modifiers(0L, tpnme.EMPTY, _), name, tparams, vparamss, tpt: TypeTree, rhs)
-//          if !tree.symbol.isConstructor && tree.symbol.isPublic &&
-//            tpt.original == null && !(tpt.tpe =:= typeOf[Unit]) =>
-//          tree -> s"The `public` method `$name` should have explicit return type, `$tpt` was inferred. Please specify return type."
+            // TODO: complain only for functions that are not already in tpt.tpe.baseType...
+            //        case tree@DefDef(Modifiers(0L, tpnme.EMPTY, _), name, tparams, vparamss, tpt: TypeTree, rhs)
+            //          if !tree.symbol.isConstructor && tree.symbol.isPublic &&
+            //            tpt.original == null && !(tpt.tpe =:= typeOf[Unit]) =>
+            //          tree -> s"The `public` method `$name` should have explicit return type, `$tpt` was inferred. Please specify return type."
 
-//        case tree@ClassDef(mods, name, tparams, impl) =>
-//          checkNoReturnType(tree, tree.symbol.baseClasses, impl)
-//          tree -> s"class $name: ${tree.tpe.baseClasses}: ${tree.symbol.baseClasses}"
-//
-//        case tree@ModuleDef(mods, name, impl) =>
-//          checkNoReturnType(tree, tree.symbol.baseClasses, impl)
-//          tree -> s"object $name: ${tree.tpe.baseClasses}: ${tree.symbol.baseClasses}"
+            //        case tree@ClassDef(mods, name, tparams, impl) =>
+            //          checkNoReturnType(tree, tree.symbol.baseClasses, impl)
+            //          tree -> s"class $name: ${tree.tpe.baseClasses}: ${tree.symbol.baseClasses}"
+            //
+            //        case tree@ModuleDef(mods, name, impl) =>
+            //          checkNoReturnType(tree, tree.symbol.baseClasses, impl)
+            //          tree -> s"object $name: ${tree.tpe.baseClasses}: ${tree.symbol.baseClasses}"
 
-        case tree@IsDefinedGetExtractor(idents) =>
-          tree -> s"""You can use a patten match `${idents.mkString(",")} match (...)` instead of `isDefined... get`."""
+            case tree@IsDefinedGetExtractor(idents) =>
+              tree -> s"""You can use a patten match `${idents.mkString(",")} match (...)` instead of `isDefined... get`."""
 
-        //        case tree@Select(value, name) if name.toString == "foreach" =>
-        //          println(tree.tpe)
-        //          println(tree.tpe.prefix)
-        //          println(tree.tpe.prefixChain)
-        //          println(tree.tpe.typeSymbol)
-        //          println(tree.tpe <:< typeOf[Range])
-        //          println(tree.tpe <:< typeOf[scala.collection.immutable.Range])
-        //          tree -> s"foreach"
-      }
+            //        case tree@Select(value, name) if name.toString == "foreach" =>
+            //          println(tree.tpe)
+            //          println(tree.tpe.prefix)
+            //          println(tree.tpe.prefixChain)
+            //          println(tree.tpe.typeSymbol)
+            //          println(tree.tpe <:< typeOf[Range])
+            //          println(tree.tpe <:< typeOf[scala.collection.immutable.Range])
+            //          tree -> s"foreach"
+          }
+          classChecks ++ bodyChecks
     }
 
     override val examine: Seq[(String, Column => Position)] => Seq[(Position, Message)] = xs => {
